d85b2c13c6f3f45ff8ff770bd5e636188f1559b0
Steve Byrne <sbb@penguinis.org>
2015-08-12 20:20:06 -0700

    WIP: add plots and computations for the second part of the questions.

--- code.R
+++ code.R
@@ -11,7 +11,25 @@ steps.by.date <- aggregate(steps ~ date, data = data, FUN = sum)
 
 #p <- qplot(data.with.valid.steps$date, data.with.valid.steps$steps, geom="histogram")
 #p <- qplot(data.with.valid.steps$steps, geom="histogram")
+if (FALSE) {
 p <- ggplot(steps.by.date, aes(x=steps)) +  geom_histogram(binwidth = 1000)  + 
-  labs(title = "Histogram showing steps per day frequencies", x = "Steps per day", y = "Frequency of days occurring")
+  labs(title = "Histogram showing steps per day frequencies", x = "Steps per day", y = "Frequency of days occurrence")
 
 print(p)
+}
+
+average.steps <- aggregate(steps ~ interval, data = data, FUN = mean)
+
+p <- ggplot(average.steps, aes(x = interval, y = steps)) + 
+  geom_line() +
+  labs(title = "Average number of steps, by interval during the day", 
+       x = "Inteval in the day", y = "Average number of steps")
+
+print(p)
+
+max.average.steps <- average.steps[which.max(average.steps$steps),]
+print(max.average.steps$interval)
+
+
+dates <- seq(ISOdate(2000, 1, 1, 0, 0, 0), ISOdate(2000, 1, 2, 0, 0,0), 60)
+print(dates[max.average.steps$interval])
